a7d5c9ca78dfc2287c405c302d102264eab84c75
Umbrien <kalugin.serej@gmail.com>
2023-08-14 20:25:46 +0300

    top-up internalisation

--- src/pages/top-up.tsx
+++ src/pages/top-up.tsx
@@ -5,14 +5,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { AuthInput } from "@/components/auth/Input";
 import { api } from "@/utils/api";
 import { useAuth } from "@/hooks/useAuth";
+import { useTranslations } from "next-intl";
 
 const validationSchema = z.object({
-  amount: z.number().positive({ message: "Amount must be positive" }),
+  amount: z.number().positive({ message: "amount-positive" }),
 });
 
 type ValidationSchema = z.infer<typeof validationSchema>;
 
 export default function TopUp() {
+  const t = useTranslations("pages.Top-Up");
   const {
     register,
     handleSubmit,
@@ -47,7 +49,7 @@ export default function TopUp() {
     <div className="flex min-h-[var(--h-antinav)] flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-700">
-          Top Up
+          {t("title")}
         </h2>
         <form
           className="mt-8 space-y-6"
@@ -58,24 +60,25 @@ export default function TopUp() {
               id="amount"
               type="number"
               autoComplete="top-up-amount"
-              placeholder="Amount"
+              placeholder={t("amount")}
               {...register("amount", { valueAsNumber: true })}
             />
           </div>
           {Object.keys(errors).length > 0 && (
             <div className="text-sm text-red-500">
               {Object.values(errors).map((error) => (
-                <div key={error.message}>{error.message}</div>
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                <div key={error.message}>{t(error.message)}</div>
               ))}
             </div>
           )}
           <button
             type="submit"
-            // disabled={!isValid || Object.keys(errors).length > 0}
             disabled={!isValid && Object.keys(errors).length > 0}
             className="group relative flex w-full justify-center rounded-md border border-transparent bg-primary-600 px-4 py-2 text-sm font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-primary-200 disabled:text-gray-600"
           >
-            {isValid ? "Top up" : "Fill in the form"}
+            {isValid ? t("top-up-btn") : t("fill-in-the-form")}
           </button>
           {mutationError && (
             <div className="text-sm text-red-500">{mutationError}</div>
@@ -85,7 +88,7 @@ export default function TopUp() {
               !toppedUpAmount && "invisible"
             }`}
           >
-            Successfully topped up {toppedUpAmount}
+            {t("successfully-topped-up", { amount: toppedUpAmount })}
           </div>
         </form>
       </div>
